565a96e1b0d8be2b1b7c96395fe6765678c68fa8
rjzhb <rjzhb222@163.com>
2022-12-23 13:11:02 +0800

    first commit

---
+++ cmake-build-debug/CMakeFiles/OSManager.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OSManager.dir/main.cpp.o"
+  "OSManager"
+  "OSManager.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OSManager.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/OSManager.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for OSManager.
+# This may be replaced when dependencies are built.
